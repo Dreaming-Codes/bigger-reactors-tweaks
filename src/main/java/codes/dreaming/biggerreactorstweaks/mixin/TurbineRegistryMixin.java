dc0bb3a5c656512bda1770aa7713d2a7d859e28a
DreamingCodes <dev@dreaming.codes>
2022-10-23 08:58:45 +0200

    Added Turbine bypass and bumped version

--- src/main/java/codes/dreaming/biggerreactorstweaks/mixin/TurbineRegistryMixin.java
+++ src/main/java/codes/dreaming/biggerreactorstweaks/mixin/TurbineRegistryMixin.java
@@ -1,14 +1,14 @@
 package codes.dreaming.biggerreactorstweaks.mixin;
 
 import net.minecraft.block.Block;
-import net.roguelogix.biggerreactors.registries.ReactorModeratorRegistry;
+import net.roguelogix.biggerreactors.registries.TurbineCoilRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ReactorModeratorRegistry.class)
-public class ReactorRegistryMixin {
+@Mixin(TurbineCoilRegistry.class)
+public class TurbineRegistryMixin {
     @Inject(at = @At("RETURN"), method = "isBlockAllowed(Lnet/minecraft/block/Block;)Z", cancellable = true, remap = false)
     private static void onIsBlockAllowed(Block block, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
